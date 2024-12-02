882add7a6b54d589c3f141dcd90f21a833aef231
HasiniE2006 <hasini1530@gmail.com>
2024-12-02 14:44:40 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+public class Datatypes {
+    public static void main(String args[])
+    {
+        int a=16;
+        short b=1530;
+        long c=1524;
+        char d='H';
+        float f=3.14f;
+        double g=3.14;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+    
+    
+        
+        
+    }
+    
+}
