baa3cb388c07b027de758838e2c681f747d67654
HasiniE2006 <hasini1530@gmail.com>
2024-12-02 15:02:10 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,12 @@
+public class Typeconversion {
+    public static void main(String[] args) {
+        //conversion of int to char
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.err.println(a);
+        System.err.println(b);
+        System.err.println(c);
+    }
+    
+}
